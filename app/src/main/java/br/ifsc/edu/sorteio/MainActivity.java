663d185e98461bf7397e87de872bc29ef510c19d
aluno <l.gustavds45@gmail.com>
2020-03-05 09:58:55 -0300

    Initial commit

---
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -0,0 +1,44 @@
+package br.ifsc.edu.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    EditText editTextInicial ;
+    EditText editTextFinal   ;
+    TextView TextViewResultado;
+    Random gerador = new Random();
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextInicial   = findViewById(R.id.editTextInicial  );
+        editTextFinal     = findViewById(R.id.editTextFinal    );
+        TextViewResultado = findViewById(R.id.textViewResultado);
+        TextViewResultado.setText(" ");
+
+
+    }
+
+    public void sortear(View view)
+    {
+        Button button = (Button) view;
+        button.setText("Sortear");
+        int inicial = Integer.parseInt(editTextInicial.getText().toString());
+        int fina = Integer.parseInt(editTextFinal.getText().toString());
+        int random = gerador.nextInt((fina - inicial) + 1) + inicial;
+        TextViewResultado.setText(Integer.toString(random));
+
+    }
+}
